696d634ee0031ed9b689c200f6b43edf41e735f6
Jee Graeff <jag918@nyu.edu>
2018-08-23 16:15:40 -0400

    create

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::MessagesController < ApplicationController
 
   def show
     response = {'message':'FAILED'}
-    @sender= User.find(params[:id]).to_i
+    @sender= User.find(params[:id])
     if @sender
       @messages= @sender.messages
       puts "______________________________"
@@ -12,5 +12,22 @@ class Api::V1::MessagesController < ApplicationController
     end
     render json: response
   end
+
+  def create
+    response = {'message':'FAILED'}
+    @message = Message.create(content: message_params[:content], sender_id: message_params[:user_id])
+    if @message.valid?
+      @message.save
+      response = {'message':'SUCCESS'}
+    end
+    render json: response
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :user_id)
+  end
+
 
 end
